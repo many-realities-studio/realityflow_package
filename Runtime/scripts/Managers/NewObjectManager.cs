0ce52d9e96801f2c1d56d64072fc29efc4958c77
Matthew Kurtz <MatthewRyanKurtz@gmail.com>
2020-02-29 18:21:11 -0500

    Created a object manager and a class of methods to interface with the networking portion of the Plugin

---
+++ Runtime/scripts/Managers/NewObjectManager.cs
@@ -0,0 +1,48 @@
+﻿using RealityFlow.Plugin.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Packages.realityflow_package.Runtime.scripts.Managers
+{
+    public class NewObjectManager
+    {
+        public static Dictionary<string, GameObject> idToGameObjectMapping = new Dictionary<string, GameObject>();
+        public static GameObject Sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        public static void CreateObjectInUnity(FlowTObject flowObject)
+        {
+            GameObject newGameObject = new GameObject();
+
+            flowObject.SavePropertiesToGameObject(newGameObject);
+            idToGameObjectMapping.Add(flowObject.flowId, newGameObject);
+        }
+
+        public static void EditObject(FlowTObject newValues)
+        {
+            //GameObject unityGameObject = idToGameObjectMapping[newValues.id];
+
+            GameObject unityGameObject = idToGameObjectMapping[newValues.flowId];
+
+            newValues.SavePropertiesToGameObject(unityGameObject);
+
+            //unityGameObject.transform.position = Sphere.transform.position;
+
+            //GameObject flowGameObject = Sphere;
+
+            // Sets all properties of the unityGameObject to the new values defined 
+            // by the flowGameObject
+            //PropertyInfo[] properties = typeof(GameObject).GetProperties();
+            //foreach (PropertyInfo property in properties)
+            //{
+            //    if(property.PropertyType == typeof(Transform))
+            //    {
+            //        property.SetValue(flowGameObject.transform.position, unityGameObject.transform.position);
+            //    }
+            //}
+        }
+    }
+}
---
+++ Runtime/scripts/Operations.cs
@@ -0,0 +1,58 @@
+﻿using Packages.realityflow_package.Runtime.scripts.Messages;
+using Packages.realityflow_package.Runtime.scripts.Messages.UserMessages;
+//using RealityFlow.Plugin.Scripts.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packages.realityflow_package.Runtime.scripts
+{
+    /// <summary>
+    /// The purpose of this class is to provide a wrapper for the UnityPlugin,
+    /// allowing for easy use with the networking tools
+    /// </summary>
+    public static class Operations
+    {
+        private static FlowWebsocket _flowWebsocket;
+        public static FlowWebsocket FlowWebsocket { get => _flowWebsocket; }
+
+        public static void Login(string username, string password, Login_Recieved.LoginReceived_EventHandler callbackFunction)
+        {
+            Login_SendToServer loginMessage = new Login_SendToServer(username, password);
+            FlowWebsocket.SendMessage(loginMessage);
+
+            Login_Recieved.ReceivedEvent += callbackFunction;
+        }
+
+        //public static void exampleCallbackFunction(object sender, ConfirmationMessageEventArgs eventArgs)
+        //{
+        //    if(eventArgs.message.WasSuccessful)
+        //    {
+        //        // Login Succeeded! :D
+        //    }
+        //    else
+        //    {
+        //        // Login Failed :(
+        //    }
+        //}
+
+        //public static void exampleLinkingFunction()
+        //{
+        //    Login("username", "password", exampleCallbackFunction);
+        //}
+        
+        public static void Logout()
+        {
+            Logout_SendToServer logoutMessage = new Logout_SendToServer();
+
+            //flowWebsocket.SendMessage(logoutMessage);
+        }
+
+        public static void ConnectToServer(string url)
+        {
+            _flowWebsocket = new FlowWebsocket(url);
+        }
+    }
+}
