9fbe0e663885b8c2c9a5479c3973ebb9019e6f43
Matthew Kurtz <MatthewRyanKurtz@gmail.com>
2020-03-27 21:54:43 -0400

    Custom inspector for flow objects

---
+++ Runtime/scripts/FlowObject_Monobehaviour.cs
@@ -0,0 +1,35 @@
+﻿using RealityFlow.Plugin.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Packages.realityflow_package.Runtime.scripts
+{
+    [ExecuteAlways]
+    [Serializable]
+    public class FlowObject_Monobehaviour : MonoBehaviour
+    {
+        public FlowTObject underlyingFlowObject;
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnValidate()
+        {
+            Debug.Log("OnValidate");
+        }
+
+        public void Update()
+        {
+            if (underlyingFlowObject != null)
+            {
+                underlyingFlowObject.UpdateObjectGlobally(underlyingFlowObject);
+            }
+        }
+    }
+}
