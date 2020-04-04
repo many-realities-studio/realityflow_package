3a27adaf4d751cbd9e42b81daf94e1159e4af64b
Matthew Kurtz <MatthewRyanKurtz@gmail.com>
2020-04-04 16:37:46 -0400

    Behaviours now have actions which are inherited to have properties

---
+++ Runtime/scripts/Behaviours/Actions/FlowAction_Teleport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packages.realityflow_package.Runtime.scripts.Messages.BehaviourMessages
+{
+    public class FlowAction_Teleport : BaseFlowAction
+    {
+        TeleportCoordinates _TeleportCoordinates;
+
+        public FlowAction_Teleport(TeleportCoordinates TeleportCoordinates) : base()
+        {
+            _TeleportCoordinates = TeleportCoordinates;
+            ActionType = "Teleport";
+        }
+    }
+}
