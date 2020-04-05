f93607e5136741c5c1a285f0b9a5e9db08d7e64c
Matthew Kurtz <MatthewRyanKurtz@gmail.com>
2020-04-05 15:08:59 -0400

    Added FlowActions and server API changes

---
+++ Runtime/scripts/Structures/Actions/TeleportAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packages.realityflow_package.Runtime.scripts.Structures.Actions
+{
+    public class TeleportAction
+    {
+        TeleportCoordinates teleportCoordinates;
+
+        public TeleportAction(TeleportCoordinates teleportCoordinates)
+        {
+            this.teleportCoordinates = teleportCoordinates;
+        }
+    }
+}
