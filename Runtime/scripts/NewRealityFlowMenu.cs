8c45104b8f478e0c9a991b2ca60924ce3a08ae27
VictorSolal <victorsolal@knights.ucf.edu>
2021-04-16 16:03:48 -0400

    Revert "Changed localhost to ngrok"
    This reverts commit 27a5aeb91f08f3ee056ee9b6009c08f4d64fbfb8.

--- Runtime/scripts/NewRealityFlowMenu.cs
+++ Runtime/scripts/NewRealityFlowMenu.cs
@@ -16,7 +16,7 @@ using TMPro;
 public class NewRealityFlowMenu : MonoBehaviour
 {
    //private string _Url = "ws://plato.mrl.ai:8999";
-    private string _Url = "ws://dc9f3aff9592.ngrok.io"; // the one we are using
+    private string _Url = "ws://localhost:8999"; // the one we are using
     private const string Url = "ws://a73c9fa8.ngrok.io";
 
     // Keyboard
